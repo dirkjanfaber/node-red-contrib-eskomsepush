3707f6b5268a003568395c05cb66e301143b5df4
Dirk-Jan Faber <dfaber@victronenergy.com>
2023-05-02 09:40:58 +0200

    Initial files
    EskomSePush API node for Node-RED.

---
+++ README.md
@@ -0,0 +1,85 @@
+### EskomSePush API
+
+![EskomsePush API](img/eskomsepush-flow.png)
+
+A node for retrieving info from the EskomSePush API.
+
+The EskomSePush-API node gives the tools to make working with the load shedding in South Africa as easy as possible.
+
+First you need to configure the node by entering the license key and entering the correct area.
+
+Once deployed, the node will fetch the data from EskomSePush every hour. As every fetch from the API takes 2 calls, the 50 free queries per day on a free account should suffice. Every ten minutes the API status is checked to see how many queries you have left.
+
+Internally the node checks every minute if a schedule is currently active or not. It will also output a message on the first deployment.
+
+### Configuration
+
+First you will need a _licence key_. You can get one from [here](https://eskomsepush.gumroad.com/l/api), by subsribing to the Free model. Note that this is for personal use only.
+
+Next you need to insert the correct area. Once you entered 5 characters (and a valid license is used), the API will be used for searching the correct area. Do note that this will cost some of the daily queries. If you don't want that and you already know the id of the area, fill out the area first and then the license key.
+
+Then you need to fill out which status to follow. This can be either _National_ (eskom) or _Capetown_.
+
+If the _test_ checkbox has been selected, test data for the area will be fetched instead of the actual schedule. This is useful when debugging.
+
+### Outputs
+
+The first output of the node outputs a boolean value. When load shedding is active, the `msg.payload` will be _true_, otherwise it will be _false_. It also outputs some extra values:  
+```
+{
+  "payload":false,
+  "LoadShedding":{
+    "schedule":{
+       "next":{
+         "start":1683561600000,
+         "end":1683570600000,
+         "type":"schedule"
+       },
+       "active":false
+    },
+    "event":{
+      "next":{
+        "start":1683561600000,
+        "end":1683570600000
+      },
+      "active":false
+    },
+    "checked":"13:35",
+    "next":{
+      "start":1683561600000,
+      "end":1683570600000,
+      "type":"schedule"
+    },
+    "active":false
+  },
+  "stage":"5",
+  "statusselect":"capetown",
+  "api":{
+    "count":12,
+    "limit":50,
+    "lastStatusUpdate":"Mon May 08 2023 13:34:30 GMT+0200 (Central European Summer Time)",
+    "lastScheduleUpdate":"Mon May 08 2023 13:34:30 GMT+0200 (Central European Summer Time)"
+  },
+  "\_msgid":"6e77b593b7f9eda4"
+}
+```
+
+The start and end objects contain the time as unix timestamp in the Javascript format (milliseconds after the epoch).
+
+The second output shows `msg.stage` and `msg.schedule`, containing the latest information as retrieved from the API. This output is mainly useful when writing your own functions and logic.
+
+### Status
+
+The status will show the situation regarding the API calls and when the next
+shedding wil start or end.  It also shows the count of API calls that have been
+done and how many are left. This updates every 10 minutes.
+
+
+### Documentation
+
+Documentation for the API can be found [here](https://documenter.getpostman.com/view/1296288/UzQuNk3E)
+
+When quota has been exceeded:
+```
+{"error":"Quota Exceeded - Reminder: you can use the 'test' query param for development. Check the docs! \ud83d\ude05"}
+```
---
+++ src/nodes/eskomsepush.js
@@ -0,0 +1,218 @@
+module.exports = function (RED) {
+  'use strict'
+
+  const axios = require('axios')
+  let EskomSePushAPI = null
+  let Stage = null
+  let Schedule = null
+  let lastStatusUpdate = new Date()
+  let lastStageUpdate = new Date()
+  let lastScheduleUpdate = new Date()
+  let LoadShedding = false
+  let fill = 'green'
+  let shape = 'ring'
+  
+  function checkAllowance (node) {
+    const options = {}
+    const headers = { token: node.config.licensekey }
+    axios.get('https://developer.sepush.co.za/business/2.0/api_allowance',
+      { params: options, headers }).then(function (response) {
+        EskomSePushAPI = response.data
+    })
+      .catch(error => {
+        node.warn({ error: error.message })
+      })
+  }
+
+  function checkStage (node) {
+    const options = {}
+    const headers = { token: node.config.licensekey }
+    axios.get('https://developer.sepush.co.za/business/2.0/status',
+      { params: options, headers }).then(function (response) {
+        Stage = response.data
+    })
+      .catch(error => {
+        node.warn({ error: error.message })
+      })
+  }
+
+  function checkSchedule (node) {
+    const options = { id: node.config.area }
+    const headers = { token: node.config.licensekey }
+    let url = 'https://developer.sepush.co.za/business/2.0/area'
+    if (node.config.test) {
+      options.test = 'current'
+    }
+    axios.get(url,
+      { params: options, headers }).then(function (response) {
+        Schedule = response.data
+        Schedule.info.area = node.config.area
+    })
+      .catch(error => {
+        node.warn({ error: error.message })
+      })
+  }
+
+  function updateStatus(node) {
+    const now = new Date();
+    let statusText = ''
+
+    if (EskomSePushAPI === null || (now.getTime() - lastStatusUpdate.getTime()) > 600000) {
+      checkAllowance(node)
+      lastStatusUpdate = now
+    }
+
+    if (Schedule && Schedule.info.area !== node.config.area) {
+      Schedule = null
+    }
+
+    if (EskomSePushAPI && EskomSePushAPI.allowance.count >= EskomSePushAPI.allowance.limit) {
+      statusText += 'API quota reached'
+      fill = 'red'
+      shape = 'dot'
+
+    } else {
+      if (Stage === null || (now.getTime() - lastStatusUpdate.getTime()) > 3600000) {
+        node.status({fill: 'yellow', shape, text: 'Fetching status'})
+        checkStage(node)
+        lastStageUpdate = now
+      }
+
+      if (Schedule === null || (now.getTime() - lastScheduleUpdate.getTime()) > 3600000) {
+        node.status({fill: 'yellow', shape, text: 'Fetching schedule'})
+        checkSchedule(node)
+        lastScheduleUpdate = now
+      }
+    }  
+
+    if (Stage && Schedule && EskomSePushAPI) {
+      let stage = Stage.status[node.config.statusselect].stage
+      let nowtime = now.toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit'})
+      let LoadShedding = {
+        schedule: {
+          next: {},
+          active: false
+        },
+        event: {
+          next: {},
+          active: false
+        },
+        checked: nowtime
+      }
+      fill = 'green'
+      for (let schedule of Schedule.schedule.days[0].stages[stage-1]) {
+        if ( nowtime >= schedule.split('-')[0] && nowtime <= schedule.split('-')[1] ) {
+          LoadShedding.schedule = {
+            active: true,
+            start: Date.parse(Schedule.schedule.days[0].date + ' ' + schedule.split('-')[0]),
+            end: Date.parse(Schedule.schedule.days[0].date + ' ' + schedule.split('-')[1])
+          }
+        }
+        if ( Object.keys(LoadShedding.schedule.next).length === 0 && nowtime < schedule.split('-')[0] ) {
+          LoadShedding.schedule.next = {
+            start: Date.parse(Schedule.schedule.days[0].date + ' ' + schedule.split('-')[0]),
+            end: Date.parse(Schedule.schedule.days[0].date + ' ' + schedule.split('-')[1])
+          }
+        }
+      }
+      if (Object.keys(LoadShedding.schedule.next).length === 0) {
+        let s = Schedule.schedule.days[1].stages[stage-1][0]
+        LoadShedding.schedule.next = {
+          start: Date.parse(Schedule.sschedule.days[1].date + s.split('-')[0]),
+          end: Date.parse(Schedule.sschedule.days[1].date + s.split('-')[1])
+        }
+      }
+      LoadShedding.event.next = {
+        start: Date.parse(Schedule.events[0].start),
+        end: Date.parse(Schedule.events[0].end)
+      }
+      if ( nowtime >= LoadShedding.event.start && nowtime < LoadShedding.event.end ) {
+        LoadShedding.event.active = true
+        LoadShedding.start = LoadShedding.event.start
+        LoadShedding.end = LoadShedding.event.end
+      }
+
+      if (!LoadShedding.schedule.next.start || !LoadShedding.event.next.start) {
+        node.warn("Unable to find next scheduled event and/or schedule")
+        return
+      }
+      if (LoadShedding.schedule.next.start <= LoadShedding.event.next.start) {
+        LoadShedding.next = LoadShedding.schedule.next
+        LoadShedding.next.type = 'schedule'
+      } else {
+        LoadShedding.next = LoadShedding.event.next
+        LoadShedding.next.type = 'event'
+      }
+
+      LoadShedding.active = (LoadShedding.schedule.active || LoadShedding.event.active)
+      if (LoadShedding.active) {
+        statusText += 'Until: '+ new Date(LoadShedding.end).toLocaleTimeString()
+        fill = 'red'
+      } else {
+        statusText += 'Next: ' + new Date(LoadShedding.next.start).toLocaleTimeString()
+      }
+      node.send([{ 
+        payload: LoadShedding.active,
+        LoadShedding,
+        stage,
+        statusselect: node.config.statusselect,
+        api: {
+          count: EskomSePushAPI.allowance.count,
+          limit: EskomSePushAPI.allowance.limit,
+          lastStatusUpdate: lastStatusUpdate.toString(),
+          lastScheduleUpdate: lastScheduleUpdate.toString()
+        }
+      }, {
+        stage: Stage,
+        schedule: Schedule }])
+    }
+
+    if (EskomSePushAPI) {
+      statusText += ` (API: ${EskomSePushAPI.allowance.count}/${EskomSePushAPI.allowance.limit})`
+    }
+
+    node.status({
+      fill, shape, text: ( statusText || 'Ok' )
+    })
+  }
+
+  function EskomSePush (config) {
+    RED.nodes.createNode(this, config)
+
+    const node = this
+    node.config = config
+
+    updateStatus(node)
+    const intervalId = setInterval(function () {
+      updateStatus(node)
+    }, 60000)
+
+    node.on('close', function () {
+      clearInterval(intervalId)
+    })
+  }
+
+  RED.nodes.registerType('eskomsepush', EskomSePush)
+
+  RED.httpNode.get('/eskomsepush/search', (req, res) => {
+    if (!req.query || !req.query.token || !req.query.search) {
+      res.setHeader('Content-Type', 'application/json')
+      return res.send('invalid')
+    }
+    const headers = {
+      token: req.query.token
+    }
+    const options = {
+      text: req.query.search
+    }
+
+    res.setHeader('Content-Type', 'application/json')
+    axios.get('https://developer.sepush.co.za/business/2.0/areas_search',
+      { params: options, headers }).then(function (response) {
+      return res.send(response.data)
+    })
+      .catch(error => {
+        return res.send({ error: error.message })
+      })
+  })
+}
