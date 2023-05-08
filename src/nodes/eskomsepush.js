6f210c107f4423f2b0e32bc01ff87b7fcaf52e9c
Dirk-Jan Faber <dfaber@victronenergy.com>
2023-05-08 21:39:30 +0200

    Another fix for parsing next days schedule
    This failed because of a missing space.

--- src/nodes/eskomsepush.js
+++ src/nodes/eskomsepush.js
@@ -116,8 +116,8 @@ module.exports = function (RED) {
       if (Object.keys(LoadShedding.schedule.next).length === 0) {
         const s = Schedule.schedule.days[1].stages[stage - 1][0]
         LoadShedding.schedule.next = {
-          start: Date.parse(Schedule.schedule.days[1].date + s.split('-')[0]),
-          end: Date.parse(Schedule.schedule.days[1].date + s.split('-')[1])
+          start: Date.parse(Schedule.schedule.days[1].date + ' ' + s.split('-')[0]),
+          end: Date.parse(Schedule.schedule.days[1].date + ' ' + s.split('-')[1])
         }
       }
       LoadShedding.event.next = {
@@ -132,6 +132,7 @@ module.exports = function (RED) {
 
       if (!LoadShedding.schedule.next.start || !LoadShedding.event.next.start) {
         node.warn('Unable to find next scheduled event and/or schedule')
+        node.warn(LoadShedding)
         return
       }
       if (LoadShedding.schedule.next.start <= LoadShedding.event.next.start) {
